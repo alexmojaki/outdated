449c7d59debbaeda4975cbb158b2991969e9cd79
Alex Hall <alex.mojaki@gmail.com>
2018-04-09 10:37:53 +0200

    Fix description and add classifiers in setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,22 @@ from outdated import __version__
 
 setup(name='outdated',
       version=__version__,
-      description='Small personal collection of python utility functions',
+      description='Check if a version of a PyPI package is outdated',
       url='https://github.com/alexmojaki/outdated',
       author='Alex Hall',
       author_email='alex.mojaki@gmail.com',
       license='MIT',
       packages=['outdated'],
+      classifiers=[
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+      ],
       zip_safe=False)
